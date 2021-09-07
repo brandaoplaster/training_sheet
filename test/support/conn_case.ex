f979c9e29c2e002c82bc9b09b93f388b27faf015
brandaoplaster <brandaoplaster@gmail.com>
2021-09-06 23:06:51 -0300

    style: formatting code

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -17,6 +17,8 @@ defmodule TrainingSheetWeb.ConnCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       # Import conveniences for testing with connections
@@ -32,10 +34,10 @@ defmodule TrainingSheetWeb.ConnCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(TrainingSheet.Repo)
+    :ok = Sandbox.checkout(TrainingSheet.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(TrainingSheet.Repo, {:shared, self()})
+      Sandbox.mode(TrainingSheet.Repo, {:shared, self()})
     end
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
