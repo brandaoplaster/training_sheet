fd0c4659e0069fed8ba99746a3c25c097de6c284
brandaoplaster <brandaoplaster@gmail.com>
2021-10-18 21:38:29 -0300

    chore: Adding query and mutation in root

---
+++ lib/training_sheet_web/schema.ex
@@ -0,0 +1,13 @@
+defmodule TrainingSheetWeb.Schema do
+  use Absinthe.Schema
+
+  import_types TrainingSheetWeb.Schema.Types.Root
+
+  query do
+    import_fields :root_query
+  end
+
+  mutation do
+    import_fields :root_mutation
+  end
+end
--- lib/training_sheet_web/schema/types/root.ex
+++ lib/training_sheet_web/schema/types/root.ex
@@ -1,9 +1,21 @@
 defmodule TrainingSheetWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
 
+  alias TrainingSheetWeb.Resolvers.User, as: UserResolver
+
   import_types TrainingSheetWeb.Schema.Types.User
 
-  obeject :root_query do
+  object :root_query do
+    field :get_user, type: :user do
+      arg :id, non_null(:uuid4)
+      resolve &UserResolver.get/2
+    end
+  end
 
+  object :root_mutation do
+    field :create_user, type: :user do
+      arg :input, non_null(:create_user)
+      resolve &UserResolver.create/2
+    end
   end
 end
